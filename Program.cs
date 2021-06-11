68a54a5de8682a805d38a45887cc24399caf448b
kamil <kamil.starzyk@currenda.pl>
2021-06-11 08:43:43 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace MyUltimateSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Hi Kamil");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(rand.Next(100,999));
+            }
+        }
+    }
+}
